4f2bcbb06c60cd9a9c1df91b50f10b667d2cfa10
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2020-03-09 17:07:31 -0400

    added routing to the webpage

---
+++ src/components/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default class NotFound extends React.Component {
+    render() {
+        return (<h1>Not found</h1>);
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Connect from './components/Connect';
+import Receive from './components/Receive';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// https://codeburst.io/getting-started-with-react-router-5c978f70df91
+const routing = (
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/connect" component={Connect} />
+                <Route path="/receive" component={Receive} />
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
