4d466cf5fca112dd941ca7b5d4ed6850313da232
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2020-03-16 19:41:50 -0400

    added css to words

--- peers/src/components/Words.js
+++ peers/src/components/Words.js
@@ -1,12 +1,42 @@
 import React from 'react'
 const datamuse = require('datamuse');
 
+const button = {
+    background: '#ffffff',
+    // display: 'flex',
+    border: '2px #0e2b4d solid',
+    alignSelf: 'center',
+    fontSize: '90%',
+    borderRadius: '10px',
+    textAlign: 'center',
+    color: '#1d7786',
+    fontFamily: `'Poppins', sans-serif`,
+    padding: '10px 10px',
+}
+
+const wordIs = {
+    display: 'flex',
+    flexDirection: 'column',
+    fontSize: '150%',
+}
+
+const guessWord = {
+    display: 'flex',
+    flexDirection: 'column',
+    fontSize: '200%',
+    color: '#ff0260',
+    fontWeight: 'bold',
+    fontFamily: `'Raleway', sans-serif`,
+}
+
+// stores all the words recieved from the API
+// -> reduces API calls
 const words = [];
 
 export default class Words extends React.Component {
     constructor() {
         super();
-        this.state = {word: ''}; // holds the recently popped word
+        this.state = { word: '' }; // holds the recently popped word
         this.getWords(); // populates words
         this.handleClick = this.handleClick.bind(this);
     }
@@ -38,10 +68,15 @@ export default class Words extends React.Component {
     render() {
         return (
             <div>
-                <button type="button" id="new_word" onClick={this.handleClick}>
-                    Show me a new Word!
-                </button>
-                <div>Word is.. {this.state.word}</div>
+                <div style={wordIs}>
+                    Word is.. 
+                    <div id="guess-word" style={guessWord}>
+                        {this.state.word}
+                    </div>
+                    <button type="button" id="new_word" style={button} onClick={this.handleClick}>
+                        Show me a new Word!
+                    </button>
+                </div>
             </div>
         );
     }
