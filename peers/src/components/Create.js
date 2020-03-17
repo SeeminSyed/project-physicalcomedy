9d719c59fff54f1a3f68e5d48373021fb175a1fa
SeeminSyed <seemin711@gmail.com>
2020-03-17 13:23:51 -0400

    fixed bug with chrome cam

--- peers/src/components/Create.js
+++ peers/src/components/Create.js
@@ -127,17 +127,7 @@ class Create extends React.Component {
         this.setState({ modelLoaded: true })
 
         // Initialize application by requesting your own video to test !
-        this.requestLocalVideo({
-            success: (stream) => {
-                this.localStream = stream;
-                this.onReceiveStream(stream, 'my-camera');
-                this.streamFeed();
-            },
-            error: (err) => {
-                alert("Cannot get access to your camera and video !");
-                console.error(err);
-            }
-        });
+        this.requestLocalVideo();
 
         // Show the ID that allows other user to connect to your session.
         this.peer.on('open', (id) => {
@@ -188,11 +178,49 @@ class Create extends React.Component {
 
     // Starts the request of the camera and microphone
     requestLocalVideo(callbacks) {
-        // Monkeypatch for crossbrowser geusermedia
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        console.log('requesting video');
+        // // Monkeypatch for crossbrowser geusermedia
+        // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-        // Request audio an video
-        navigator.getUserMedia({ audio: true, video: true }, callbacks.success, callbacks.error);
+        // // Request audio an video
+        // navigator.getUserMedia({ audio: true, video: true }, callbacks.success, callbacks.error);
+
+        // Older browsers might not implement mediaDevices at all, so we set an empty object first
+        if (navigator.mediaDevices === undefined) {
+            navigator.mediaDevices = {};
+        }
+
+        // Some browsers partially implement mediaDevices. We can't just assign an object
+        // with getUserMedia as it would overwrite existing properties.
+        // Here, we will just add the getUserMedia property if it's missing.
+        if (navigator.mediaDevices.getUserMedia === undefined) {
+            navigator.mediaDevices.getUserMedia = (constraints) => {
+
+                // First get ahold of the legacy getUserMedia, if present
+                let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+                // Some browsers just don't implement it - return a rejected promise with an error
+                // to keep a consistent interface
+                if (!getUserMedia) {
+                    return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+                }
+
+                // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+                return new Promise(function (resolve, reject) {
+                    getUserMedia.call(navigator, constraints, resolve, reject);
+                });
+            }
+        }
+
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream) => {
+            this.localStream = stream;
+            this.onReceiveStream(stream, 'my-camera');
+            this.streamFeed();
+        }).catch((err) => {
+            alert("Cannot get access to your camera and video !");
+            console.error(err);
+
+        })
     }
 
     // Handle the providen stream (video and audio) to the desired video element
@@ -201,7 +229,11 @@ class Create extends React.Component {
         let video = document.getElementById(element_id);
         // Set the given stream as the video source
         // video.src = window.URL.createObjectURL(stream);
+        console.log('stream', stream);
         video.srcObject = stream;
+        video.onloadedmetadata = function (e) {
+            video.play();
+        };
     }
 
     // provides a list of hand position predicitons from a source
@@ -227,7 +259,7 @@ class Create extends React.Component {
                     let video = document.getElementById('my-camera');
 
                     this.drawDoodle(context);
-                    
+
                     this.runDetection(video);
                     // let feed = document.getElementById('feed');
                     // this.runDetection(feed);
