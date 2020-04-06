c1686eeab6a2b6db2880a4c3300ffafba1019b6f
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2020-04-06 19:36:39 -0400

    added one of the components for home page

--- server/peer-server.js
+++ server/peer-server.js
@@ -1,5 +1,5 @@
 // project/server/peer-server.js
-const fs = require('fs');
+const fs = require('fs'); // ?
 const PeerServer = require('peer').PeerServer;
 
 const server = PeerServer({
@@ -10,3 +10,9 @@ const server = PeerServer({
     //     cert: fs.readFileSync('../server.crt', 'utf8') // server crt
     // }
 });
+
+console.log("Server has started on PORT 9000");
+
+/// TODO: Make this into an express server
+/// Implement a words api --> call datamuse api here..
+/// hopefully that'll make the CORs (C A N C E R) issue go away..
