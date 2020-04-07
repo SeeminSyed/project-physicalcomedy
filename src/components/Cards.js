8f4831be257be0719b47d76862edad79b3660470
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2020-04-07 12:19:33 -0400

    fixed the validation bug

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -6,25 +6,116 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup'
 
-export default class Cards extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            validate: 'null'
-        };
-    }
+function CardOneForm() {
+    const [validate, setValidate] = useState(false);
 
-    handleSubmit(event) { // TODO: fix this
-        console.log(event);
+    const handleSubmit = (event) => {
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
-            this.setState({ validate: 'false' });
             event.preventDefault();
             event.stopPropagation();
-
         }
-        this.setState({ validate: 'true' });
-    }
+
+        setValidate(true);
+    };
+
+    return (
+        <Form noValidate validated={validate} onSubmit={handleSubmit} action="/charades">
+            {/* enter a word */}
+            <Form.Group as={Col} controlId="enter-word">
+                <Form.Label sm="10">Enter a word:</Form.Label>
+                <Col sm="10">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">w</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control type="text" placeholder="Enter word" required />
+                        <Form.Control.Feedback type="invalid">Please enter a word.</Form.Control.Feedback>
+                    </InputGroup>
+                </Col>
+            </Form.Group>
+            {/* Choose a category */}
+            <Form.Group as={Col} controlId="category">
+                <Form.Label sm="10">Choose a word category:</Form.Label>
+                <Col sm="10">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">c</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control as="select">
+                            <option>Nouns</option>
+                            <option>Adjectives</option>
+                            <option>Verbs</option>
+                            <option>Adverbs</option>
+                            <option>Synonyms</option>
+                            <option>Antonyms</option>
+                            <option>Rhymes</option>
+                            <option>Homophones</option>
+                        </Form.Control>
+                        <Form.Control.Feedback type="invalid">Please choose an option.</Form.Control.Feedback>
+                    </InputGroup>
+                </Col>
+            </Form.Group>
+            {/* Score */}
+            <Form.Group as={Col} controlId="score">
+                <Form.Label sm="10">Score:</Form.Label>
+                <Col sm="10">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control
+                            type="number"
+                            min='5'
+                            placeholder="Score"
+                            aria-describedby="inputGroupPrepend"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">Please enter a valid score.</Form.Control.Feedback>
+                    </InputGroup>
+                </Col>
+            </Form.Group>
+            <Button type="submit" variant="primary">Create Room</Button> {/*href="/charades"*/}
+        </Form>
+    );
+
+}
+
+function CardTwoForm() {
+    const [validate, setValidate] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidate(true);
+    };
+
+    return (
+        <Form noValidate validated={validate} onSubmit={handleSubmit} action="/pictionary">
+            <Form.Group as={Col} controlId="enter-word">
+                <Form.Label sm="10">Enter a Code:</Form.Label>
+                <Col sm="10">
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <Form.Control type="text" placeholder="Enter code" required />
+                        <Form.Control.Feedback type="invalid">
+                            Please enter a code.
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                </Col>
+            </Form.Group>
+            <Button type="submit" variant="primary">Join a Room!</Button>
+        </Form>
+    );
+}
+
+export default class Cards extends React.Component {
 
     render() {
         return (
@@ -40,63 +131,7 @@ export default class Cards extends React.Component {
                         <Card.Body>
                             <Card.Text>
                                 Create a new room and get your mates to join in!
-                                    <Form validated={this.state.validate} onSubmit={this.handleSubmit}>
-                                    {/* enter a word */}
-                                    <Form.Group as={Col} controlId="enter-word">
-                                        <Form.Label sm="10">Enter a word:</Form.Label>
-                                        <Col sm="10">
-                                            <InputGroup>
-                                                <InputGroup.Prepend>
-                                                    <InputGroup.Text id="inputGroupPrepend">w</InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <Form.Control type="text" placeholder="Enter word" required />
-                                                <Form.Control.Feedback type="invalid">Please enter a word.</Form.Control.Feedback>
-                                            </InputGroup>
-                                        </Col>
-                                    </Form.Group>
-                                    {/* Choose a category */}
-                                    <Form.Group as={Col} controlId="category">
-                                        <Form.Label sm="10">Choose a word category:</Form.Label>
-                                        <Col sm="10">
-                                            <InputGroup>
-                                                <InputGroup.Prepend>
-                                                    <InputGroup.Text id="inputGroupPrepend">c</InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <Form.Control as="select">
-                                                    <option>Nouns</option>
-                                                    <option>Adjectives</option>
-                                                    <option>Verbs</option>
-                                                    <option>Adverbs</option>
-                                                    <option>Synonyms</option>
-                                                    <option>Antonyms</option>
-                                                    <option>Rhymes</option>
-                                                    <option>Homophones</option>
-                                                </Form.Control>
-                                                <Form.Control.Feedback type="invalid">Please choose an option.</Form.Control.Feedback>
-                                            </InputGroup>
-                                        </Col>
-                                    </Form.Group>
-                                    {/* Score */}
-                                    <Form.Group as={Col} controlId="score">
-                                        <Form.Label sm="10">Score:</Form.Label>
-                                        <Col sm="10">
-                                            <InputGroup>
-                                                <InputGroup.Prepend>
-                                                    <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
-                                                </InputGroup.Prepend>
-                                                <Form.Control
-                                                    type="number"
-                                                    min='5'
-                                                    placeholder="Score"
-                                                    aria-describedby="inputGroupPrepend"
-                                                    required
-                                                />
-                                                <Form.Control.Feedback type="invalid">Please enter a valid score.</Form.Control.Feedback>
-                                            </InputGroup>
-                                        </Col>
-                                    </Form.Group>
-                                    <Button type="submit" href="/charades" variant="primary">Create Room</Button>
-                                </Form>
+                                    <CardOneForm />
                             </Card.Text>
                         </Card.Body>
                     </Accordion.Collapse>
@@ -113,23 +148,7 @@ export default class Cards extends React.Component {
                             <Card.Text>
                                 Join a room and start playing with your mates!
                         </Card.Text>
-                            <Form>
-                                <Form.Group as={Col} controlId="enter-word">
-                                    <Form.Label sm="10">Enter a Code:</Form.Label>
-                                    <Col sm="10">
-                                        <InputGroup>
-                                            <InputGroup.Prepend>
-                                                <InputGroup.Text id="inputGroupPrepend">#</InputGroup.Text>
-                                            </InputGroup.Prepend>
-                                            <Form.Control type="text" placeholder="Enter code" required />
-                                            <Form.Control.Feedback type="invalid">
-                                                Please enter a code.
-                                        </Form.Control.Feedback>
-                                        </InputGroup>
-                                    </Col>
-                                </Form.Group>
-                            </Form>
-                            <Button type="submit" href="/pictionary" variant="primary">Join a Room!</Button>
+                            <CardTwoForm />
                         </Card.Body>
                     </Accordion.Collapse>
                 </Card>
