c9ebd6cdc11deb7a2701722fbfc07245728a8cb5
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2020-04-08 00:10:47 -0400

    changed file structure, added express server and words endpoint

---
+++ app.js
@@ -0,0 +1,70 @@
+// project/server/peer-server.js
+
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const datamuse = require('datamuse');
+
+// serve React files
+app.use(express.static('static'));
+
+app.get('/', (req, res, next) => res.send('Hello world!'));
+
+function getCode(type) {
+    let code = '';
+    switch (type) {
+        case 'Noun':
+            code = 'rel_jja';
+            break;
+        case 'Adjectives':
+            code = 'rel_jjb';
+            break;
+        case 'Synonyms':
+            code = 'rel_syn';
+            break;
+        case 'Antonyms':
+            code = 'rel_ant';
+            break;
+        case 'Rhymes':
+            code = 'rel_rhy';
+            break;
+        case 'Homophones':
+            code = 'rel_hom';
+            break;
+    }
+    return code;
+}
+
+function populateArray(json) {
+    let words = [];
+    let word;
+    for (let i = 0; i < json.length; i++) {
+        word = json[i].word;
+        words.push(word);
+    }
+    return words;
+}
+
+app.get('/words/:type/:word', function (req, res, next) {
+    let code = getCode(req.params.type);
+    datamuse.request(`words?${code}=${req.params.word}`).then((json) => {
+        let words = populateArray(json);
+        res.json(words);
+    });
+});
+
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+    path: '/myapp'
+});
+app.use('/peerjs', peerServer);
+server.listen(PORT, function (err) {
+    if (err) console.log(err);
+    else console.log("HTTP server on http://localhost:%s", PORT);
+});
