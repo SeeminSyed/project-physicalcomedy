c5f85b2043aa1c979c68fafd45d949401c17c79f
sakina.gadriwala <sakina.gadriwala@mail.utoronto.ca>
2020-04-09 01:18:29 -0400

    finished credits

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 export default class Credits extends React.Component {
     render() {
         return (
             <div className="App">
-                {/* TODO: Complete this page */}
-                <text>Hello!</text>
+                <Header />
+                <h3>Credits</h3>
+                <p style={{margin:"20px", fontSize:"25px"}}>
+                    Thanks to these amazing projects, developing <a href="/">Physical Comedy</a> was a piece of cake <span role="img" aria-label="piece of cake">🍰</span>.
+                    <ul style={{margin:"10px"}}>
+                        <li> <a rel="noopener noreferrer" target="_blank" href="https://thierrysans.me/CSCC09/">CSCC09</a> Professor Thierry Sans and his Teaching Team </li>
+                        <li> <a rel="noopener noreferrer" target="_blank" href="https://datamuse.com/api/">Datamuse</a></li>
+                        <li> <a rel="noopener noreferrer" target="_blank" href="https://peerjs.com/">Peerjs</a> Documentation</li>
+                        <li> <a rel="noopener noreferrer" target="_blank" href="http://reactjs.org/">Reactjs</a></li>
+                        <li> <a rel="noopener noreferrer" target="_blank" href="https://fonts.google.com/">Google Fonts</a></li>
+                        <li><a rel="noopener noreferrer" target="_blank" href="https://react-bootstrap.netlify.com/">Bootstrap for React</a></li>
+                        <li> <a rel="noopener noreferrer" target="_blank" href="https://stackoverflow.com/">Stack Overflow</a></li>
+                        <li>Gesture Detection</li>
+                        <li> Many <a rel="noopener noreferrer" target="_blank" href="https://hackernoon.com"> Hackernoon</a> and <a rel="noopener noreferrer" target="_blank" href="http://medium.com/"> Medium </a> Articles</li>
+                        <li><a rel="noopener noreferrer" target="_blank" href="http://codepen.io/">Codepen.io </a></li>
+                        <li><a rel="noopener noreferrer" target="_blank" href="https://heroku.com">Heroku</a></li>
+                        {/* <li></li> Peerjs Articles? */}
+                    </ul>
+                </p>
+                <Footer />
             </div>
         );
     }
 }
-
-// https://styled-components.com/
-// https://emoji-css.afeld.me/
