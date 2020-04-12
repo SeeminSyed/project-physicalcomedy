e6a079914013c132b68c4159db40cf392f115803
SeeminSyed <seemin711@gmail.com>
2020-04-11 23:53:40 -0400

    updated credits with changes from another branch

--- static/src/pages/Credits.js
+++ static/src/pages/Credits.js
@@ -27,34 +27,31 @@ export default class Credits extends React.Component {
                     }}>
                     <Col>
                         <h3>Credits</h3>
-                        <div style={{ margin: "20px", fontSize: "20px", }}>
+                        <p style={{ margin: "20px", fontSize: "20px", }}>
                             Thanks to these amazing projects, design components, UI elements and snippets of code, developing <a href="/">Physical Comedy</a> was a piece of cake <span role="img" aria-label="piece of cake">🍰</span>.
                                 <ul style={{ margin: "10px" }}>
                                 <Row>
                                     <Col>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://thierrysans.me/CSCC09/">CSCC09</a> Professor Thierry Sans and his Teaching Team </li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://datamuse.com/api/">Datamuse</a></li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://peerjs.com/">Peerjs</a> Documentation</li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="http://reactjs.org/">Reactjs</a></li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://fonts.google.com/">Google Fonts</a></li>
-                                        <li><a rel="noopener noreferrer" target="_blank" href="https://react-bootstrap.netlify.com/">Bootstrap for React</a></li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://stackoverflow.com/">Stack Overflow</a></li>
-                                        <li> Many <a rel="noopener noreferrer" target="_blank" href="https://hackernoon.com"> Hackernoon</a> and <a rel="noopener noreferrer" target="_blank" href="http://medium.com/"> Medium </a> Articles</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://thierrysans.me/CSCC09/">CSCC09</a> Professor Thierry Sans and his Teaching Team. </li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://datamuse.com/api/">Datamuse</a>, a public API for words.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://peerjs.com/">Peerjs</a> and <a rel="noopener noreferrer" target="_blank" href="https://www.npmjs.com/package/peer">Peer</a> Documentation.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="http://reactjs.org/">Reactjs</a> for front-end.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://fonts.google.com/">Google Fonts</a>.</li>
                                         <li><a rel="noopener noreferrer" target="_blank" href="http://codepen.io/">Codepen.io </a></li>
-                                        <li><a rel="noopener noreferrer" target="_blank" href="https://heroku.com">Heroku</a> and <a rel="noopener noreferrer" target="_blank" href="https://daveceddia.com/deploy-react-express-app-heroku/"> Dave Ceddia's</a> article on deployment of React and Express Applications</li>
-                                        <li>Gesture Detection, our npm packages</li>
+                                        <li><a rel="noopener noreferrer" target="_blank" href="https://react-bootstrap.netlify.com/">Bootstrap for React</a>.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/API">MDN Web Documentation</a>.</li>
                                     </Col>
                                     <Col>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="/"></a> https://github.com/ourcodeworld/videochat-peerjs-example/blob/master/public/source/js/script.js </li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="/"></a> https://www.andismith.com/blogs/2012/07/extending-getusermedia/ </li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="/"></a> https://stackoverflow.com/questions/56704138/i-want-to-create-a-copy-to-clipboard-using-react-js </li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="/"></a> https://www.cometchat.com/tutorials/build-an-anonymous-chat-app-with-react-react-bootstrap/ </li>
-                                        <li> <a rel="noopener noreferrer" target="_blank" href="/"></a> https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia basically developer.mozilla site </li>
-                                        
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://stackoverflow.com/">Stack Overflow</a> to help us with debugging.</li>
+                                        <li> Many <a rel="noopener noreferrer" target="_blank" href="https://hackernoon.com"> Hackernoon</a> and <a rel="noopener noreferrer" target="_blank" href="http://medium.com/"> Medium </a> Articles.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://github.com/ourcodeworld/videochat-peerjs-example/blob/master/public/source/js/script.js">Video Chat</a> Peerjs Example and <a rel="noopener noreferrer" target="_blank" href="https://www.andismith.com/blogs/2012/07/extending-getusermedia/">Andi Smith's</a> Article.</li>
+                                        <li><a rel="noopener noreferrer" target="_blank" href="https://heroku.com">Heroku</a> and <a rel="noopener noreferrer" target="_blank" href="https://daveceddia.com/deploy-react-express-app-heroku/"> Dave Ceddia's</a> article on deployment of React and Express Applications.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://www.npmjs.com/package/handtrackjs">Gesture Detection</a>, a npm package to help us with gesture detection.</li>
+                                        <li> <a rel="noopener noreferrer" target="_blank" href="https://www.cometchat.com/tutorials/build-an-anonymous-chat-app-with-react-react-bootstrap/">CometChat</a>, a reference project.</li>
                                     </Col>
                                 </Row>
                             </ul>
-                        </div>
+                        </p>
                     </Col>
                 </div>
                 <Footer />
